feac6887396987ba0c1db81210302cddbb645e8b
joseguevara29 <guevarabus29@gmail.com>
2021-05-24 20:08:46 -0400

    finished the styling of the class form

--- src/components/ClassesHomePage.js
+++ src/components/ClassesHomePage.js
@@ -3,29 +3,22 @@
 
 //route path '/protected'
 
+import React, { useState } from "react";
 const ClassesHomePage = () => {
+  const [classList, setClassList] = useState({
+    class_id: 1,
+    class_name: "Full Body Athlete",
+    type: "HIIT",
+    date: "2021-05-20",
+    start_time: "High",
+    duration_mins: 30,
+    intensity: "High",
+    location: "Takoma Park",
+    current_registered: 5,
+    max_class_size: 15,
+  });
 
-
-
-    const [classList, setClassList] = useState({
-        class_id: 1,
-        class_name: 'Full Body Athlete',
-        type: 'HIIT',
-        date: '2021-05-20',
-        start_time: 'High',
-        duration_mins: 30,
-        intensity: 'High',
-        location: 'Takoma Park',
-        current_registered: 5,
-        max_class_size: 15
-    })
-
-    return(
-        <div>
-            Test
-        </div>
-    )
-
-}
+  return <div>Test</div>;
+};
 
 export default ClassesHomePage;
