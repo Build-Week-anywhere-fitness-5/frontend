a3f7df542f8759491072c96d079ecae5b343c567
joseguevara29 <guevarabus29@gmail.com>
2021-05-27 21:25:50 -0400

    Added nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,43 @@
-import styled from "styled-components";
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 import LogIn from "./LogIn";
 import SignUp from "./SignUp";
 import "../App.css";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  bu: {
+    margin: "auto",
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "100%",
+  },
+}));
+
 export default function NavBar(props) {
+  const classes = useStyles();
+
   return (
-    <div>
-      <nav className="nav-container">
-        <h1>Anywhere Fitness</h1>
-        <div className="nav-links">
-          <Link className="nav-button" to="/">
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Button color="inherit" className={classes.bu} href="/protected">
+            Home
+          </Button>
+          <Button color="inherit" className={classes.bu} href="/signup">
+            SignUp
+          </Button>
+          <Button color="inherit" className={classes.bu} href="/">
             Login
-          </Link>
-          <Link className="nav-button" to="/signup">
-            Sign up
-          </Link>
-        </div>
-      </nav>
+          </Button>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -5,12 +5,14 @@ import createAuthorization from "./../helpers/axiosWithAuth";
 import "./signup.css";
 
 export default function SignUp() {
+  //  axios
+
   const [state, setState] = useState({
     username: "",
     password: "",
+    role: "",
   });
 
-<<<<<<< HEAD
   const change = (a) => {
     const { id, value } = a.target;
     setState((prevState) => ({
@@ -20,52 +22,13 @@ export default function SignUp() {
   };
   return (
     <div>
-      <label for="credentials">choose type:</label>
+      <label for="role">choose type:</label>
 
-      <select id="credentials">
-        <option value="instructor">instructor</option>
-        <option value="client">client</option>
+      <select id="role" value={state.role} onChange={change}>
+        <option value="">-- Select an Option --</option>
+        <option value="client">Client</option>
+        <option value="instructor">Instructor</option>
       </select>
-
-      <input
-        type="email"
-        classname="form-control"
-        id="username"
-        placeholder="Enter email"
-        value={state.username}
-        onChange={change}
-      />
-=======
-  //  axios
-
-const [State,setState] = useState({
-  username : "",
-  password : "",
-  role: ''
-})
-
-const Change = (a) => {
-  const {id , value} = a.target
-  setState(prevState => ({
-    ...prevState,
-    [id] : value
-  }))
-}
-return(
-  <div >
-    
-    <label for="role">choose type:</label>
-
-    <select 
-        id = 'role'
-        value = {State.role}
-        onChange = {Change}
-        >
-          <option value = ''>-- Select an Option --</option>
-          <option value ='client'>Client</option>
-          <option value ='instructor'>Instructor</option>
-        </select>
->>>>>>> 5bb2d3fcacf0553f69a8f12c35cfcb530cca5798
 
       <input
         type="password"
